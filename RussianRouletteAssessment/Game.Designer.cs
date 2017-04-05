a6acc722bb09250f7a6c47be6c059ba5b4f8814c
SumOfAllN00bs <DiscordShards@gmail.com>
2017-04-06 10:52:27 +1200

    Done

--- RussianRouletteAssessment/Game.Designer.cs
+++ RussianRouletteAssessment/Game.Designer.cs
@@ -13,6 +13,10 @@
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
+            GameTextFont.Dispose();
+            buffer.Dispose();
+            pfc.Dispose();
+            sf.Dispose();
             if (disposing && (components != null))
             {
                 components.Dispose();
